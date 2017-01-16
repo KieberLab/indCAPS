80ec908e98b2036e02924c0d3badbb49908f1ad9
Charles Hodgens <hodgens@email.unc.edu>
2017-01-16 11:52:37 -0500

    January 16th, 2017 Modified main script so it is easier to use in web applications. Changed name of script from dCAPS-new-v7.py to indCAPS.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,17 +1,29 @@
 import os
 import sys
 import warnings
-import requests
+#import requests
 from math import log
+import numpy
 from flask import Flask, render_template, request
 from dCAPS-new-v7 import hammingBool, hamming, revComp, baseNumbers, deltaG, deltaS
 from dCAPS-new-v7 import saltAdjusted, basicTemp, nearestNeighbor, estimateTM
 from dCAPS-new-v7 import lastSharedBase, scanUnshared, scanSequence, evaluateSites
-from dCAPS-new-v7 import generatePrimer, compareEnzymes, makePrimer
+from dCAPS-new-v7 import generatePrimer, compareEnzymes, makePrimer, enzymes
 
 # Set up Flask stuff
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+
+# Import enzymes
+enzymes = {}
+with open("motifs.txt") as enzymeFile:
+	for line in enzymeFile:
+		line = line.rstrip()
+		line = line.lower()
+		if line[0] is "#":
+			continue
+		(enzyme,motif,forward,reverse) = line.split("\t")
+		enzymes[enzyme] = [motif,forward,reverse]
 
 @aap.route('/', methods=['GET','POST'])
 def index():
@@ -19,15 +31,20 @@ def index():
 	if request.method == "POST":
 		# Get stuff the user entered
 		try:
-			seq1 = request.form['seq1']
+			seq1 = request.form['seq1','seq2','ham']
 			seq2 = request.form['seq2']
-			receivedRequest = requests.get() # I think i need to look up the docs on this
+			hamDist = request.form['ham']
 		except:
 			errors.append("No sequences provided.")
-		if receivedRequest:	
-			x=1
+		allResults = []
+		if seq1 and seq2 and hamDist:
 			# Call function to evaluate enzymes
-			
+			for eachEnzyme in enzymes:
+				enzymeName = eachEnzyme
+				enzymeValue = enzymes[eachEnzyme]
+				enzymeResults = evaluateSites(seq1,seq2,enzymeValue,hamDist, enzymeName)
+				if enzymeResults is not None:
+					allResults.append(enzymeReuslts)
 			# Call function to generate primers
 			
 			# Call function to generate output
