9b598c19327d6a6e716baccdd32e049e12de6c0e
Charles Hodgens <hodgens@email.unc.edu>
2017-01-17 14:46:06 -0500

    January 17th, 2017

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
+setup(name='indCAPS',
+      version='0.1',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      author='Charles Hodgens',
+      author_email='hodgens@email.unc.edu',
+      install_requires=['Flask==0.7.2'],
      )
--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,7 @@ from indCAPS import lastSharedBase, scanUnshared, scanSequence, evaluateSites
 from indCAPS import generatePrimer, compareEnzymes, makePrimer, enzymes
 
 # Set up Flask stuff
-app = Flask(__name__,instance_path="/var/lib/openshift/587e498942d9f524f40001d7/app-root/repo")
+app = Flask(__name__)
 #app.config.from_object(os.environ['APP_SETTINGS'])
 app.jinja_env.trim_blocks = True
 
