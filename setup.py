89fd8e015a25ac232367bfbe963618584d7a804b
Charles Hodgens <hodgens@email.unc.edu>
2017-01-17 17:59:56 -0500

    January 17th, 2017

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 from setuptools import setup
 
@@ -9,15 +8,3 @@ setup(name='indCAPS',
       author_email='hodgens@email.unc.edu',
 #      install_requires=['Flask==0.10.1'],
 )
-=======
-from setuptools import setup
-
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
->>>>>>> e1f1baa6f375beb734b1abeb1ed1d58e940f1ea8
