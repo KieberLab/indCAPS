ad0a5207287f23b71705f15beb6cc90801fb7084
Charles Hodgens <hodgens@email.unc.edu>
2017-01-17 17:36:58 -0500

    January 17th, 2017

--- indCAPS.py
+++ indCAPS.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-
+# -*- coding: utf-8 -*-
 #seq1 = 'GCGGAgggcccCTCAAGATCCCGAGTgggTCTTATcccCAGTTTCTTGGCTCTGTTA' #arguments[1]
 #seq2 = 'GCGGAgggcccCTCAAGATCCCGAGTgggcccCAGTTTCTTGGCTCTGTTA' #arguments[2]
 #currentMotif = 'gggccc'
