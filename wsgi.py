c1ebb5effd6e8296c3cebd3cd2d0c4ed1e1a093b
Charles Hodgens <hodgens@email.unc.edu>
2017-01-31 22:27:22 -0500

    x

--- wsgi.py
+++ wsgi.py
@@ -62,6 +62,8 @@ def results():
 			# Assemble output using sites and primers
 			
 			# Display output
+			print(allResults)
+			print(notes)
 			return(render_template('results.html',allResults=allResults,notes=notes))
 	return(render_template('index.html')) # if you tried to go to the results page on your own rather than being sent by the index, redirect to the index page
 	
